a6db9b761354db2d163c1c92fed68efb1049984f
Amer Varajic <varajicamer@gmail.com>
2023-04-30 23:18:59 +0200

    added container & other files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+// import Article from "./components/article/article";
+// import Brand from "./components/brand/brand";
+// import CTA from "./components/cta/Cta";
+// import Feature from "./components/feature/Feature";
+// import Navbar from "./components/navbar/Navbar";
+
 export default function App() {
   return (
     <div>
