888ce061594fae872b3083774fd74a8b57fadacc
Amer Varajic <varajicamer@gmail.com>
2023-05-01 16:25:48 +0200

    Imported components & added App structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from "react";
 
-// import Article from "./components/article/article";
-// import Brand from "./components/brand/brand";
-// import CTA from "./components/cta/Cta";
-// import Feature from "./components/feature/Feature";
-// import Navbar from "./components/navbar/Navbar";
+import {
+  Footer,
+  Blog,
+  Features,
+  Possibility,
+  WhatGPT3,
+  Header,
+} from "./containers";
+import { CTA, Navbar, Brand } from "./components";
 
 export default function App() {
   return (
-    <div>
-      <h1>GPT-3</h1>
+    <div className="App">
+      <div className="gradients__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
