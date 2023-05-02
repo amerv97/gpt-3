7ef26972d5ca915f68b5fcd99f4eea905b1e0380
Amer Varajic <varajicamer@gmail.com>
2023-05-02 19:03:08 +0200

    finished whatGPT3 styling

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -4,12 +4,12 @@ import { Feature } from "../../components";
 
 const WhatGPT3 = () => {
   return (
-    <div className="gpt3__whatgpt3-section__margin" id="whpt3">
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
       <div className="gpt3__whatgpt3-feature">
         <Feature />
       </div>
       <div className="gpt3__whatgpt3-heading">
-        <h1 className="gradient-text">
+        <h1 className="gradient__text">
           The possibilities are beyond your imagination
         </h1>
         <p>Explore the Library</p>
